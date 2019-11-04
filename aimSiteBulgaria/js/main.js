af87933d5e7c9ffcc847db13afe643232e8ff6f1
aimdao <valentin.ryakhovskiy@gmail.com>
2019-11-04 21:32:33 +0400

    Плавная прокрутка вниз экрана по клику на стрелочку (aimSiteBulgaria)

--- aimSiteBulgaria/js/main.js
+++ aimSiteBulgaria/js/main.js
@@ -1,3 +1,4 @@
+//================================================================
 $(document).ready(function () {
     $('.home4-slider').slick({
         infinite: true,
@@ -15,11 +16,28 @@ $(document).ready(function () {
     });
 });
 
+//================================================================
 
 /* Modal (модальное окно bootstrap). */
-$(function(){
+$(function () {
     $('.homeH-phone__link').click(function () {
         $('#myModal').modal();
+    });
+});
+
+//================================================================
+
+/* По клику на "стрелочку", плавная прокрутка экрана вниз. */
+$(document).ready(function () {
+    var margin = 10; // переменная для контроля докрутки
+    $('.homeH-arrowButton').click(function () { // тут пишите условия, для всех ссылок или для конкретных
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top + margin + 'px' // .top+margin - ставьте минус, если хотите увеличить отступ
+        }, {
+            duration: 1500, // Тут можно задать скорость прокрутки.
+            easing: 'swing'
+        });
+        return false;
     });
 });
 

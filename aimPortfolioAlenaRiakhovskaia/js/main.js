ee78b934335e122c274a0077d4a2cdf99a9a0740
Valentin Ryakhovskiy <aimdao@Valentins-Mac-mini.local>
2022-12-18 11:58:19 +0500

    upload site aimPortfolioAlenaRiakhovskaia

---
+++ aimPortfolioAlenaRiakhovskaia/js/main.js
@@ -0,0 +1,35 @@
+/* ==*==*==*==*==*==*==*==*==*==*==*== */
+/* IMPORTS: general js */
+/* ==*==*==*==*==*==*==*==*==*==*==*== */
+//
+//
+// Slick (слайдер-карусель, source: https://kenwheeler.github.io/slick/).
+//
+// Целевой класс-родитель слайдера: restSlider-one
+// Параметры настройки слайдера в файле: slickSliderClass__restSlider-one.js
+import "./modules-slick-slider-for-different-classes/slickSliderClass__restSlider-one.js";
+//
+// Целевой класс-родитель слайдера: restSlider-two
+// Параметры настройки слайдера в файле: slickSliderClass__restSlider-two.js
+import "./modules-slick-slider-for-different-classes/slickSliderClass__restSlider-two.js";
+//
+// Целевой класс-родитель слайдера: bakerySlider-one
+// Параметры настройки слайдера в файле: slickSliderClass__bakerySlider-one.js
+import "./modules-slick-slider-for-different-classes/slickSliderClass__bakerySlider-one.js";
+//
+// Целевой класс-родитель слайдера: artBusinessSlider-one
+// Параметры настройки слайдера в файле: slickSliderClass__artBusinessSlider-one.js
+import "./modules-slick-slider-for-different-classes/slickSliderClass__artBusinessSlider-one.js";
+//
+// Целевой класс-родитель слайдера: coreSlider-one
+// Параметры настройки слайдера в файле: slickSliderClass__coreSlider-one.js
+import "./modules-slick-slider-for-different-classes/slickSliderClass__coreSlider-one.js";
+//
+// Целевой класс-родитель слайдера: tdSlider-one
+// Параметры настройки слайдера в файле: slickSliderClass__restSlider-one.js
+import "./modules-slick-slider-for-different-classes/slickSliderClass__tdSlider-one.js";
+//
+// Целевой класс-родитель слайдера: tdSlider-two
+// Параметры настройки слайдера в файле: slickSliderClass__tdSlider-two.js
+import "./modules-slick-slider-for-different-classes/slickSliderClass__tdSlider-two.js";
+//
